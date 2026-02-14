f25d042e59eebf4c786c90a9283b5a7e65c613c2
ClausCode <nclaus.code@gmail.com>
2026-02-14 22:42:32 +0300

    add routing and make auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthPage } from './pages/auth.page/auth.page';
+import { LandingPage } from './pages/landing.page/landing.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPage,
+  },
+  {
+    path: 'auth',
+    component: AuthPage,
+  },
+];
---
+++ src/app/pages/landing.page/landing.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing.page',
+  imports: [],
+  templateUrl: './landing.page.html',
+})
+export class LandingPage {}
