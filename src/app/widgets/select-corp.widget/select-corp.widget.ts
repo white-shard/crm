58c4cbc822cef79c803989dbab5545686e92da98
ClausCode <nclaus.code@gmail.com>
2026-02-15 19:42:34 +0300

    add select corp widget

---
+++ src/app/widgets/select-corp.widget/select-corp.widget.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from '../../common-ui/svg-icon/svg-icon';
+
+@Component({
+  selector: 'select-corp-widget',
+  imports: [SvgIcon],
+  templateUrl: './select-corp.widget.html',
+  styleUrl: './select-corp.widget.css',
+})
+export class SelectCorpWidget {}
